636187fb71327fa31c9253c169519a31176962bd
Coen Jonker <coen.jonker@frieslandcampina.com>
2021-12-08 13:08:10 +0100

    case 1 for day 8 done

--- day08.py
+++ day08.py
@@ -1,4 +1,17 @@
 
+SIMPLE_DIGITS = (1,4,7,8)
+
+class NumberDisplay(object):
+
+    def __init__(self, strrep):
+        self.strrep = strrep
+
+    @property
+    def bitstring(self) -> str:
+        pass
+
+    def __len__(self):
+        return len(self.strrep)
 
 
 class NumberDisplayDeducer(object):
@@ -7,4 +20,30 @@ class NumberDisplayDeducer(object):
         self.inputstr, self.outputstr = (x.strip() for x in digitstr.split("|"))
         self.inputlist = self.inputstr.split()
         self.outputlist = self.outputstr.split()
+
+    def count_in_output(self, n):
+        if n == 1:
+            return sum(len(x) == 2 for x in self.outputlist)
+        if n == 4:
+            return sum(len(x) == 4 for x in self.outputlist)
+        if n == 7:
+            return sum(len(x) == 3 for x in self.outputlist)
+        if n == 8:
+            return sum(len(x) == 7 for x in self.outputlist)
+
+
+def count_numbers_in_outputs(lines):
+    summed = 0
+
+    for line in lines:
+        if len(line) > 1:
+            print(f"LINE: '{line}'")
+            ndd = NumberDisplayDeducer(line)
+            summed += sum([ndd.count_in_output(d) for d in SIMPLE_DIGITS])
+
+    return summed
+
+if __name__ == "__main__":
+    lines = open("./input/08.txt", 'r')
+    print(count_numbers_in_outputs(lines))
 
--- test_day08.py
+++ test_day08.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from day08 import NumberDisplayDeducer
+from day08 import NumberDisplayDeducer, count_numbers_in_outputs
 
 EXAMPLE_INPUT = """
 be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
@@ -15,12 +15,15 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 """
 
+EXAMPLE_OUTPUT = 26
+
 ONE_LINE = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
 ONE_LINE_INPUT = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb"
 ONE_LINE_OUTPUT = "fdgacbe cefdb cefbgd gcbe"
 
 ONE_LINE_OUTPUT_PARSED = ["fdgacbe", "cefdb", "cefbgd", "gcbe"]
 ONE_LINE_INPUT_PARSED = ["be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb"]
+
 
 
 class TestNumberDisplayDeducer(TestCase):
@@ -37,4 +40,25 @@ class TestNumberDisplayDeducer(TestCase):
         # inputlist and outputlist should contain these outputs
         self.assertListEqual(ONE_LINE_OUTPUT_PARSED, self.ndd.outputlist)
         self.assertListEqual(ONE_LINE_INPUT_PARSED, self.ndd.inputlist)
+
+    def count_number_in_output_of_x_should_be_y(self, x, y):
+        self.assertEqual(y, self.ndd.count_in_output(x), f"Count of {x} should be {y}")
+
+    def test_count_1_in_output_should_be_0(self):
+        self.count_number_in_output_of_x_should_be_y(x=1, y=0)
+
+    def test_count_4_in_output_should_be_1(self):
+        self.count_number_in_output_of_x_should_be_y(x=4, y=1)
+
+    def test_count_7_in_output_should_be_0(self):
+        self.count_number_in_output_of_x_should_be_y(x=7, y=0)
+
+    def test_count_8_in_output_should_be_0(self):
+        self.count_number_in_output_of_x_should_be_y(x=8, y=1)
+
+
+class TestExampleCase1(TestCase):
+
+    def test_solve_example_number_count(self):
+        self.assertEqual(EXAMPLE_OUTPUT, count_numbers_in_outputs(EXAMPLE_INPUT.split("\n")))
 
