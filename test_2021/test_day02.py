b9fd8ab631c2249e31eb304f4d29a5fc3d4d653f
Coen Jonker <coen.jonker@frieslandcampina.com>
2022-11-26 15:04:09 +0100

    Day 2 done

--- test_2021/test_day02.py
+++ test_2021/test_day02.py
@@ -1,11 +1,12 @@
 import unittest
+from unittest import TestCase
 
 from aocd.models import Puzzle
 
-from y2021.day02 import solve_a, solve_b, Submarine
+from y2021.day02 import solve_a, solve_b, Submarine, extract_command_parameters, SubmarineB
 
 EXAMPLE_RESULT_A = 150
-EXAMPLE_RESULT_B = 0
+EXAMPLE_RESULT_B = 900
 
 
 class TestSubmarine(unittest.TestCase):
@@ -33,7 +34,27 @@ class TestSubmarine(unittest.TestCase):
                          f"Sub's position indicator should be 20, not {self.sub.multiplied_pos_indicator}")
 
 
-class TestDay01(unittest.TestCase):
+class TestSubmarineB(TestCase):
+
+    def setUp(self) -> None:
+        self.sub = SubmarineB()
+
+    def test_parse_down_command(self):
+        self.sub.parse_command("down 4")
+        self.assertEqual(self.sub.aim, 4)
+
+    def test_parse_up_command(self):
+        self.sub.parse_command("up 2")
+        self.assertEqual(self.sub.aim, -2)
+
+    def test_parse_forward_command(self):
+        self.sub.parse_command("down 3")
+        self.sub.parse_command("forward 3")
+        self.assertEqual(self.sub.position, 3)
+        self.assertEqual(self.sub.depth, 9)
+
+
+class TestDay02(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -51,6 +72,14 @@ class TestDay01(unittest.TestCase):
         result = solve_b(self.example_data)
         self.assertEqual(self.example_result_b, result,
                          f"Example result B should be {self.example_result_b}, not {result}")
+
+
+class TestModuleMethods(TestCase):
+    def test_extract_command_parameters(self):
+        amount, direction = extract_command_parameters("forward 5")
+        self.assertIsInstance(amount, int, "Amount should be an integer")
+        self.assertEqual(direction, "forward")
+        self.assertEqual(amount, 5)
 
 
 if __name__ == '__main__':
--- y2021/day02.py
+++ y2021/day02.py
@@ -7,38 +7,67 @@ DAY = 2
 YEAR = 2021
 
 
+def extract_command_parameters(command):
+    direction, amount_str = command.split()
+    amount = int(amount_str)
+    return amount, direction
+
+
 @dataclass
 class BaseSubmarine(ABC):
     depth: int = 0
     position: int = 0
 
-    @abstractmethod
-    def parse_command(self, command: str):
-        pass
-
     @property
     def multiplied_pos_indicator(self):
         return self.depth * self.position
 
-    def extract_command_parameters(self, command):
-        direction, amount_str = command.split()
-        amount = int(amount_str)
-        return amount, direction
+    def parse_command(self, command: str):
+        amount, direction = extract_command_parameters(command)
+        if direction == "forward":
+            self.forward(amount)
+        if direction == "down":
+            self.down(amount)
+        if direction == "up":
+            self.up(amount)
+
+    @abstractmethod
+    def forward(self, amount):
+        pass
+
+    @abstractmethod
+    def down(self, amount):
+        pass
+
+    @abstractmethod
+    def up(self, amount):
+        pass
 
 
 class Submarine(BaseSubmarine):
-    def parse_command(self, command: str):
-        amount, direction = self.extract_command_parameters(command)
-        if direction == "forward":
-            self.position += amount
-        if direction == "down":
-            self.depth += amount
-        if direction == "up":
-            self.depth -= amount
+
+    def down(self, amount):
+        self.depth += amount
+
+    def up(self, amount):
+        self.depth -= amount
+
+    def forward(self, amount):
+        self.position += amount
 
 
 class SubmarineB(BaseSubmarine):
     aim: int = 0
+
+    def forward(self, amount):
+        self.depth += self.aim * amount
+        self.position += amount
+
+    def down(self, amount):
+        self.aim += amount
+
+    def up(self, amount):
+        self.aim -= amount
 
 
 def solve_a(data):
@@ -49,7 +78,10 @@ def solve_a(data):
 
 
 def solve_b(data):
-    return None
+    s = SubmarineB()
+    for line in data.splitlines(keepends=False):
+        s.parse_command(line)
+    return s.multiplied_pos_indicator
 
 
 if __name__ == "__main__":
