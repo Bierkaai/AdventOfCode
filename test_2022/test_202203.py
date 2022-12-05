5f9c0aecebe75080ee000760c5f956dc46df16df
Coen Jonker <coen.jonker@frieslandcampina.com>
2022-12-05 21:38:51 +0100

    Day3 done (not pretty though)

--- test_2022/test_202203.py
+++ test_2022/test_202203.py
@@ -1,18 +1,128 @@
+import types
 import unittest
 
 from aocd.models import Puzzle
 
 from y2022.day03 import solve_a, solve_b
+from y2022.day03 import Rucksack, calculate_priority, split_in_groups, split_lines, find_common_item
 
 YEAR = 2022
 DAY = 3
 EXAMPLE_RESULT_A = 157
-EXAMPLE_RESULT_B = 111
+EXAMPLE_RESULT_B = 70
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 EXAMPLE_DATA = puzzle.example_data
 
-class TestDay02(unittest.TestCase):
+EXAMPLE_RUCKSACK = "vJrwpWtwJgWrhcsFMMfFFhFp"
+EXAMPLE_COMP_1 = "vJrwpWtwJgWr"
+EXAMPLE_COMP_2 = "hcsFMMfFFhFp"
+EXAMPLE_ITEMCOUNT = 24
+EXAMPLE_ITEMS_PER_COMP = 12
+EXAMPLE_COMMON_ITEM = "p"
+
+EXAMPLE_PRIORITIES = (
+    ('p', 16),
+    ('L', 38),
+    ('P', 42),
+    ('v', 22)
+)
+
+EXAMPLE_STRING = """AAA
+BBB
+CCC
+DDD
+EEE
+FFF
+GGG
+"""
+
+EXAMPLE_SPLITLINES = [
+    "AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG"
+]
+
+EXAMPLE_GROUPS_4 = [
+    ("AAA", "BBB", "CCC", "DDD"),
+    ("EEE", "FFF", "GGG")
+]
+
+EXAMPLE_GROUPS_3 = [
+    ("AAA", "BBB", "CCC"),
+    ("DDD", "EEE", "FFF"),
+    ("GGG",)
+]
+
+EXAMPLE_GROUP_PARSED = [
+    ("vJrwpWtwJgWrhcsFMMfFFhFp",
+     "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+     "PmmdzqPrVvPwwTWBwg"),
+    ("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+     "ttgJtRGJQctTZtZT",
+     "CrZsJsPPZsGzwwsLwLmpwMDw",)
+]
+
+FIRST_GROUP_COMMON_ITEM = 'r'
+SECOND_GROUP_COMMON_ITEM = 'Z'
+
+
+class TestRucksack(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.r = Rucksack(EXAMPLE_RUCKSACK)
+
+    def test_itemcount(self):
+        self.assertEqual(EXAMPLE_ITEMCOUNT, self.r.itemcount)
+
+    def test_items_per_compartment(self):
+        self.assertEqual(EXAMPLE_ITEMS_PER_COMP, self.r.items_per_compartment)
+
+    def test_split_compartments(self):
+        self.assertEqual(EXAMPLE_COMP_1, self.r.compartments[0])
+        self.assertEqual(EXAMPLE_COMP_2, self.r.compartments[1])
+
+    def test_find_common_item(self):
+        self.assertEqual(EXAMPLE_COMMON_ITEM, self.r.find_common_item_in_all_compartments())
+
+
+class TestComputePriority(unittest.TestCase):
+
+    def test_prio_p(self):
+        for i, case in enumerate(EXAMPLE_PRIORITIES):
+            with self.subTest(i=i):
+                c, prio = case
+                self.assertEqual(prio, calculate_priority(c))
+
+
+class TestGroupRucksacks(unittest.TestCase):
+
+    def test_splitlines(self):
+        result = split_lines(EXAMPLE_STRING)
+        self.assertIsInstance(result, types.GeneratorType)
+        self.assertListEqual(EXAMPLE_SPLITLINES, list(result))
+
+    def test_split_in_groups_3(self):
+        result = split_in_groups(EXAMPLE_SPLITLINES, 3)
+        self.assertIsInstance(result, types.GeneratorType)
+        self.assertListEqual(EXAMPLE_GROUPS_3, list(result))
+
+    def test_split_in_groups_4(self):
+        result = split_in_groups(EXAMPLE_SPLITLINES, 4)
+        self.assertIsInstance(result, types.GeneratorType)
+        self.assertListEqual(EXAMPLE_GROUPS_4, list(result))
+
+    def test_split_example(self):
+        result = list(split_in_groups(split_lines(EXAMPLE_DATA), 3))
+        self.assertListEqual(EXAMPLE_GROUP_PARSED, result)
+
+    def test_find_common_item_in_rucksacks(self):
+        self.assertEqual(find_common_item(EXAMPLE_GROUP_PARSED[0]), FIRST_GROUP_COMMON_ITEM)
+        self.assertEqual(find_common_item(EXAMPLE_GROUP_PARSED[1]), SECOND_GROUP_COMMON_ITEM)
+
+
+
+
+
+class TestDay03(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -30,6 +140,7 @@ class TestDay02(unittest.TestCase):
         result = solve_b(self.example_data)
         self.assertEqual(self.example_result_b, result,
                          f"Example result B should be {self.example_result_b}, not {result}")
+
 
 if __name__ == '__main__':
     unittest.main()
--- y2022/day03.py
+++ y2022/day03.py
@@ -1,21 +1,76 @@
-import numpy as np
 from aocd.models import Puzzle
+from string import ascii_letters
+from routines.lists import generate_sublists
 
 DAY = 3
 YEAR = 2022
 
+prio_map = {
+    letter: i + 1
+    for i, letter in enumerate(ascii_letters)
+}
+
+
+class Rucksack:
+
+    def __init__(self, packstr, n_comp=2):
+        self.packstr = packstr
+        self.itemcount = len(packstr)
+        self.n_comp = n_comp
+        self.items_per_compartment = int(len(self.packstr) / self.n_comp)
+        self.compartments = {}
+        self.recompute_compartments()
+
+    def recompute_compartments(self):
+        for i in range(self.n_comp):
+            self.compartments[i] = self.packstr[
+                                   i * self.items_per_compartment
+                                   :(i + 1) * self.items_per_compartment
+                                   ]
+
+    def find_common_item_in_all_compartments(self):
+        return set.intersection(*[set(v) for v in self.compartments.values()]).pop()
+
+
+def calculate_priority(character):
+    return prio_map[character]
+
+
+def split_lines(data):
+    for line in data.splitlines(keepends=False):
+        yield line
+
+
+def initiate_rucksacks(data):
+    return [Rucksack(s) for s in split_lines(data)]
+
+
+def split_in_groups(lines, items_per_group):
+    return generate_sublists(lines, items_per_group, truncate=True)
+
+
+def find_common_item(rucksacks):
+    return set.intersection(*[set(r) for r in rucksacks]).pop()
 
 
 def solve_a(data):
-    pass
+    rucksacks = initiate_rucksacks(data)
+    return sum([
+        calculate_priority(r.find_common_item_in_all_compartments())
+        for r in rucksacks
+    ])
+
 
 def solve_b(data):
-    pass
+    rucksack_teams = split_in_groups(split_lines(data), 3)
+    return sum([
+        calculate_priority(find_common_item(team_sacks))
+        for team_sacks in rucksack_teams
+    ])
+
 
 if __name__ == "__main__":
     p = Puzzle(day=DAY, year=YEAR)
     input_data = p.input_data
     p.answer_a = solve_a(input_data)
     p.answer_b = solve_b(input_data)
-
-
