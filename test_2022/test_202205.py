b641634988c528560c7d9757d09175078b0ee5ec
Coen Jonker <coen.jonker@frieslandcampina.com>
2022-12-05 14:02:20 +0100

    Day 5 done

--- test_2022/test_202205.py
+++ test_2022/test_202205.py
@@ -2,15 +2,107 @@ import unittest
 
 from aocd.models import Puzzle
 
-from y2022.day03 import solve_a, solve_b
+from y2022.day05 import parse_and_split_input, DockCrane, DockCrane9001, parse_move_instruction
+from y2022.day05 import solve_a, solve_b
 
 YEAR = 2022
-DAY = 3
-EXAMPLE_RESULT_A = None
-EXAMPLE_RESULT_B = None
+DAY = 5
+EXAMPLE_RESULT_A = "CMZ"
+EXAMPLE_RESULT_B = "MCD"
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 EXAMPLE_DATA = puzzle.example_data
+
+EXAMPLE_DOCK = [
+    "    [D]    ",
+    "[N] [C]    ",
+    "[Z] [M] [P]",
+    " 1   2   3 "
+]
+
+EXAMPLE_MOVE = "move 2 from 1 to 3"
+EXAMPLE_MOVE_PARSED = (2, 1, 3)
+
+EXAMPLE_MOVES = ["move 1 from 2 to 1", "move 3 from 1 to 3", "move 2 from 2 to 1", "move 1 from 1 to 2"]
+DOCK = {1: ["N", "Z"],
+        2: ["D", "C", "M"],
+        3: ["P"]
+        }
+
+EXAMPLE_DOCK_MOVE_1_FROM_2_TO_3 = {
+    1: ["N", "Z"],
+    2: ["C", "M"],
+    3: ["D", "P"]
+}
+
+EXAMPLE_DOCK_MOVE_2_FROM_1_TO_3 = {
+    1: [],
+    2: ["D", "C", "M"],
+    3: ["Z", "N", "P"]
+}
+
+EXAMPLE_DOCK_MOVE_2_FROM_1_TO_3_9001 = {
+    1: [],
+    2: ["D", "C", "M"],
+    3: ["N", "Z", "P"]
+}
+
+EXAMPLE_READ_TOPLINE = "NDP"
+
+
+class TestParser(unittest.TestCase):
+
+    def setUp(self):
+        self.dock, self.moves = parse_and_split_input(EXAMPLE_DATA)
+
+    def test_parse_split_dock_on_newline(self):
+        self.assertEqual(EXAMPLE_DOCK, self.dock)
+
+    def test_parse_split_moves_on_newLine(self):
+        self.assertEqual(EXAMPLE_MOVES, self.moves)
+
+    def test_parse_move_instruction(self):
+        output_move_parsed = parse_move_instruction(EXAMPLE_MOVE)
+        self.assertTupleEqual(EXAMPLE_MOVE_PARSED, output_move_parsed)
+
+
+class TestCrane9001(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.dc = DockCrane9001(EXAMPLE_DOCK)
+
+    def test_move_two_crates(self):
+        self.dc.move(n=2, source=1, target=3)
+        self.assertDictEqual(EXAMPLE_DOCK_MOVE_2_FROM_1_TO_3_9001, self.dc.dock)
+
+    def test_move_single_crate(self):
+        self.dc.move(n=1, source=2, target=3)
+        self.assertDictEqual(EXAMPLE_DOCK_MOVE_1_FROM_2_TO_3, self.dc.dock)
+
+
+class TestCrane(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.dc = DockCrane(EXAMPLE_DOCK)
+
+    def test_dock_parsing(self):
+        self.assertDictEqual(DOCK, self.dc.dock)
+
+    def test_read_topline(self):
+        self.assertEqual(EXAMPLE_READ_TOPLINE, self.dc.read_topline())
+
+    def test_move_single_crate(self):
+        self.dc.move(n=1, source=2, target=3)
+        self.assertDictEqual(EXAMPLE_DOCK_MOVE_1_FROM_2_TO_3, self.dc.dock)
+
+    def test_move_two_crates(self):
+        self.dc.move(n=2, source=1, target=3)
+        self.assertDictEqual(EXAMPLE_DOCK_MOVE_2_FROM_1_TO_3, self.dc.dock)
+
+    def test_do_move_from_str(self):
+        self.dc.move_from_str(EXAMPLE_MOVE)
+        self.assertDictEqual(EXAMPLE_DOCK_MOVE_2_FROM_1_TO_3, self.dc.dock)
+
 
 class TestDay02(unittest.TestCase):
 
@@ -30,6 +122,7 @@ class TestDay02(unittest.TestCase):
         result = solve_b(self.example_data)
         self.assertEqual(self.example_result_b, result,
                          f"Example result B should be {self.example_result_b}, not {result}")
+
 
 if __name__ == '__main__':
     unittest.main()
--- y2022/day05.py
+++ y2022/day05.py
@@ -1,21 +1,78 @@
-import numpy as np
+from typing import Tuple, List
+
 from aocd.models import Puzzle
 
-DAY = 3
+DAY = 5
 YEAR = 2022
 
 
+def parse_and_split_input(raw_input: str) -> Tuple[List[str], List[str]]:
+    dock = []
+    moves = []
+    doing_dock = True
+    for line in raw_input.splitlines(keepends=False):
+        if doing_dock and len(line) > 0:
+            dock.append(line)
+        elif len(line) == 0:
+            doing_dock = False
+        else:
+            moves.append(line)
+    return dock, moves
+
+
+def parse_move_instruction(move_str: str) -> Tuple[int, int, int]:
+    tokenized = move_str.split()
+    return tuple(int(tokenized[x]) for x in (1, 3, 5))
+
+
+class DockCrane:
+
+    def __init__(self, dockdesc: List[str]):
+        self.dock_nrs = dockdesc[-1]
+        self.dock = dict()
+        docklines = dockdesc[:-1]
+        for pos, nr in enumerate(self.dock_nrs):
+            try:
+                self.dock[int(nr)] = [line[pos] for line in docklines if line[pos] != " "]
+            except ValueError:
+                continue
+
+    def read_topline(self):
+        return "".join([x[0] for x in self.dock.values()])
+
+    def move(self, n, source, target):
+        for _ in range(n):
+            self.dock[target].insert(0, self.dock[source].pop(0))
+
+    def move_from_str(self, move_str):
+        self.move(*parse_move_instruction(move_str))
+
+
+class DockCrane9001(DockCrane):
+
+    def move(self, n, source, target):
+        self.dock[target] = self.dock[source][:n] + self.dock[target]
+        del self.dock[source][:n]
+
 
 def solve_a(data):
-    pass
+    dock, moves = parse_and_split_input(data)
+    d = DockCrane(dock)
+    for move_str in moves:
+        d.move_from_str(move_str)
+    return d.read_topline()
+
 
 def solve_b(data):
-    pass
+    dock, moves = parse_and_split_input(data)
+    d = DockCrane9001(dock)
+    for move_str in moves:
+        d.move_from_str(move_str)
+    return d.read_topline()
+
 
 if __name__ == "__main__":
     p = Puzzle(day=DAY, year=YEAR)
     input_data = p.input_data
     p.answer_a = solve_a(input_data)
     p.answer_b = solve_b(input_data)
-
-
