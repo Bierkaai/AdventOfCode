4acf572e362296a0fb3d4a867433a74c4db2e654
Coen Jonker <coen.jonker@frieslandcampina.com>
2022-12-05 22:34:40 +0100

    Day 4 done

--- test_2022/test_202204.py
+++ test_2022/test_202204.py
@@ -2,17 +2,59 @@ import unittest
 
 from aocd.models import Puzzle
 
-from y2022.day03 import solve_a, solve_b
+from y2022.day04 import make_set, make_two_sets, fully_contains
+from y2022.day04 import solve_a, solve_b, overlaps
 
 YEAR = 2022
-DAY = 3
-EXAMPLE_RESULT_A = None
-EXAMPLE_RESULT_B = None
+DAY = 4
+EXAMPLE_RESULT_A = 2
+EXAMPLE_RESULT_B = 4
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 EXAMPLE_DATA = puzzle.example_data
 
-class TestDay02(unittest.TestCase):
+FIRST_LINE = "2-4,6-8"
+FIRST_LINE_SETS = ({2, 3, 4}, {6, 7, 8})
+
+SIMPLE_RANGE_STR = "4-9"
+SIMPLE_RANGE_SET = {4, 5, 6, 7, 8, 9}
+
+SUBSET_TESTS = [
+    ([{2, 3, 4}, {2, 3}], True),
+    ([{7, 8}, {3, 4, 5, 6, 7, 8, 9}], True),
+    ([{6, 7}, {3, 4, 5, 6, 7, 8}], True),
+    ([{1, 2, 3}, {4, 5, 6}], False),
+    ([{1, 2, 3}, {1, 2, 3}], True)
+]
+
+OVERLAPS_TESTS = [
+    ([{1, 2, 3}, {3, 4, 5}], True),
+]
+
+
+class TestSectionLogic(unittest.TestCase):
+
+    def test_parse_input(self):
+        self.assertTupleEqual(FIRST_LINE_SETS, make_two_sets(FIRST_LINE))
+
+    def test_make_set(self):
+        self.assertSetEqual(SIMPLE_RANGE_SET, make_set(SIMPLE_RANGE_STR))
+
+    def test_determine_subsets(self):
+        self.assertTrue(fully_contains({2, 3, 4}, {2, 3}))
+        for i, testcase in enumerate(SUBSET_TESTS):
+            with self.subTest(i=i):
+                case, expected_result = testcase
+                self.assertEqual(expected_result, fully_contains(*case))
+
+    def test_overlap(self):
+        for i, testcase in enumerate(SUBSET_TESTS + OVERLAPS_TESTS):
+            with self.subTest(i=i):
+                case, expected_result = testcase
+                self.assertEqual(expected_result, overlaps(*case))
+
+
+class TestDay04(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -30,6 +72,7 @@ class TestDay02(unittest.TestCase):
         result = solve_b(self.example_data)
         self.assertEqual(self.example_result_b, result,
                          f"Example result B should be {self.example_result_b}, not {result}")
+
 
 if __name__ == '__main__':
     unittest.main()
--- y2022/day04.py
+++ y2022/day04.py
@@ -1,21 +1,54 @@
-import numpy as np
 from aocd.models import Puzzle
 
-DAY = 3
+DAY = 4
 YEAR = 2022
 
 
+def make_set(input_range_str):
+    start, end = (int(x) for x in input_range_str.split("-"))
+    return set(range(start, end + 1))
+
+
+def make_two_sets(input_line):
+    return tuple(make_set(rng) for rng in input_line.split(','))
+
+
+def fully_contains(set_1: set, set_2: set) -> bool:
+    """
+    Tests whether one set fully contains another set, bidirectionally
+
+    :param set_1: a set
+    :param set_2: another set
+    :return: True if either set fully contains the other
+    """
+    if set_1 <= set_2:
+        return True
+    if set_2 <= set_1:
+        return True
+    return False
+
+
+def overlaps(set_1: set, set_2: set) -> bool:
+    return not set_1.isdisjoint(set_2)
+
+
+def solver(data, fun):
+    return sum([
+        fun(*make_two_sets(line))
+        for line in data.splitlines(keepends=False)
+    ])
+
 
 def solve_a(data):
-    pass
+    return solver(data, fully_contains)
+
 
 def solve_b(data):
-    pass
+    return solver(data, overlaps)
+
 
 if __name__ == "__main__":
     p = Puzzle(day=DAY, year=YEAR)
     input_data = p.input_data
     p.answer_a = solve_a(input_data)
     p.answer_b = solve_b(input_data)
-
-
