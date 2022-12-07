6d678bc2d8338431adddfd2179f326bd20d73dd7
Coen Jonker <coen.jonker@frieslandcampina.com>
2022-12-07 19:48:49 +0100

    Ready to start day 7. First tests passed.

--- test_2022/test_202207.py
+++ test_2022/test_202207.py
@@ -2,17 +2,37 @@ import unittest
 
 from aocd.models import Puzzle
 
-from y2022.day03 import solve_a, solve_b
+from y2022.day07 import solve_a, solve_b, ROOT_NAME, FileSystemNode
+from y2022.day07 import FileSystemNav
+
+FS_NODE_TEST_NAME = "nodename"
 
 YEAR = 2022
-DAY = 3
-EXAMPLE_RESULT_A = None
-EXAMPLE_RESULT_B = None
+DAY = 7
+EXAMPLE_RESULT_A = 95437
+EXAMPLE_RESULT_B = 9999
 
 puzzle = Puzzle(year=YEAR, day=DAY)
 EXAMPLE_DATA = puzzle.example_data
 
-class TestDay02(unittest.TestCase):
+class TestFileSystemNav(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.fs = FileSystemNav(root = ROOT_NAME)
+
+    def test_pwd_is_root(self):
+        self.assertEqual(ROOT_NAME, self.fs.pwd)
+
+
+class TestFileSytemNode(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.n = FileSystemNode(FS_NODE_TEST_NAME)
+
+    def test_name_returns_name(self):
+        self.assertEqual(FS_NODE_TEST_NAME, self.n.name)
+
+class TestDay07(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -30,6 +50,7 @@ class TestDay02(unittest.TestCase):
         result = solve_b(self.example_data)
         self.assertEqual(self.example_result_b, result,
                          f"Example result B should be {self.example_result_b}, not {result}")
+
 
 if __name__ == '__main__':
     unittest.main()
--- y2022/day07.py
+++ y2022/day07.py
@@ -1,21 +1,33 @@
-import numpy as np
 from aocd.models import Puzzle
 
-DAY = 3
+ROOT_NAME = "/"
+
+DAY = 7
 YEAR = 2022
 
+
+class FileSystemNode:
+
+    def __init__(self, name: str):
+        self.name = name
+
+
+class FileSystemNav:
+
+    def __init__(self, root: str = ROOT_NAME):
+        self.pwd = root
 
 
 def solve_a(data):
     pass
 
+
 def solve_b(data):
     pass
+
 
 if __name__ == "__main__":
     p = Puzzle(day=DAY, year=YEAR)
     input_data = p.input_data
     p.answer_a = solve_a(input_data)
     p.answer_b = solve_b(input_data)
-
-
