e12b76f8035ea3389946f649fec92d9471fa37af
Coen Jonker <coen.jonker@frieslandcampina.com>
2022-12-07 20:27:46 +0100

    Refactoring, removing tests for abstract classes

--- test_2022/test_202207.py
+++ test_2022/test_202207.py
@@ -1,9 +1,14 @@
 import unittest
 
+
 from aocd.models import Puzzle
 
-from y2022.day07 import solve_a, solve_b, ROOT_NAME, FileSystemNode
+from y2022.day07 import solve_a, solve_b, ROOT_NAME, FileSystemNode, File, Directory
 from y2022.day07 import FileSystemNav
+
+TEST_FILE_SIZE = 142311
+
+TEST_FILE_NAME = "illegal_download.mov"
 
 FS_NODE_TEST_NAME = "nodename"
 FS_SUBDIR = "bla"
@@ -16,10 +21,13 @@ EXAMPLE_RESULT_B = 9999
 puzzle = Puzzle(year=YEAR, day=DAY)
 EXAMPLE_DATA = puzzle.example_data
 
+
 class TestFileSystemNav(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.fs = FileSystemNav(root = ROOT_NAME)
+        self.fs = FileSystemNav(root=ROOT_NAME)
+        self.fs.add_file(name=TEST_FILE_NAME, size=TEST_FILE_SIZE)
+        self.fs.add_dir(name=FS_SUBDIR)
 
     def test_pwd_is_root(self):
         self.assertIsInstance(self.fs.pwd, FileSystemNode)
@@ -31,14 +39,28 @@ class TestFileSystemNav(unittest.TestCase):
         self.assertEqual(FS_SUBDIR, pwd.name)
         self.assertIs(pwd, self.fs.pwd)
 
+    def test_size_calc(self):
+        pwd_size = self.fs.lsal()
+        self.assertEqual(pwd_size, TEST_FILE_SIZE)
 
-class TestFileSytemNode(unittest.TestCase):
+
+class testDirectory(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.n = FileSystemNode(FS_NODE_TEST_NAME)
+        self.d = Directory(name=FS_SUBDIR)
 
-    def test_name_returns_name(self):
-        self.assertEqual(FS_NODE_TEST_NAME, self.n.name)
+    def test_empty_dir_size_0(self):
+        # empty dir has size 0
+        self.assertEqual(0, self.d.total_size())
+
+class TestFile(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.f = File(name=TEST_FILE_NAME, size=TEST_FILE_SIZE)
+
+    def test_total_size(self):
+        self.assertEqual(TEST_FILE_SIZE, self.f.total_size())
+
 
 class TestDay07(unittest.TestCase):
 
--- y2022/day07.py
+++ y2022/day07.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod, ABC
+
 from aocd.models import Puzzle
 
 ROOT_NAME = "/"
@@ -6,17 +8,47 @@ DAY = 7
 YEAR = 2022
 
 
-class FileSystemNode:
+class FileSystemNode(ABC):
 
     def __init__(self, name: str):
         self.name = name
+
+    @abstractmethod
+    def total_size(self):
+        return 0
+
+class Directory(FileSystemNode):
+
+    def total_size(self):
+        return 0
+
+class File(FileSystemNode):
+
+    def __init__(self, name: str, size: int):
+        super().__init__(name)
+        self.size = size
+
+    def total_size(self):
+        return self.size
 
 
 class FileSystemNav:
 
     def __init__(self, root: str = ROOT_NAME):
-        self.root = FileSystemNode(root)
+        self.root = Directory(root)
         self.pwd = self.root
+
+    def cd(self, dir: str):
+        pass
+
+    def add_file(self, name, size):
+        pass
+
+    def add_dir(self, name):
+        pass
+
+    def lsal(self):
+        return pwd.total_size()
 
 
 def solve_a(data):
