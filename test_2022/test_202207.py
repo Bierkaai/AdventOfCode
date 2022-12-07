afc0b08a4f274642894c81dae701d9d9a562c374
Coen Jonker <coen.jonker@frieslandcampina.com>
2022-12-07 15:21:15 +0100

    Starting day 6

--- test_2022/test_202207.py
+++ test_2022/test_202207.py
@@ -5,45 +5,14 @@ from aocd.models import Puzzle
 from y2022.day03 import solve_a, solve_b
 
 YEAR = 2022
-DAY = 6
-EXAMPLE_RESULT_A = 95437
+DAY = 3
+EXAMPLE_RESULT_A = None
 EXAMPLE_RESULT_B = None
 
-#puzzle = Puzzle(year=YEAR, day=DAY)
-#EXAMPLE_DATA = puzzle.example_data
+puzzle = Puzzle(year=YEAR, day=DAY)
+EXAMPLE_DATA = puzzle.example_data
 
-EXAMPLE_DATA = """$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
-"""
-
-class TestFileSystem(unittest.TestCase):
-
-    def test_fs(self):
-        pass
-
-@unittest.skip
-class TestDay06(unittest.TestCase):
+class TestDay02(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
