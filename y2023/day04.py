6bd8e922181ebe9aa339070b77c938c44ad3c89c
Coen Jonker <coen.j.jonker@gmail.com>
2023-12-04 09:50:52 +0100

    Implementation works, but last element is ignored

--- y2023/day04.py
+++ y2023/day04.py
@@ -6,19 +6,23 @@ YEAR = 2023
 
 PATTERN = r"(?:Card\s?)([0-9]+)(?:\:\s*)((?:[0-9]+\s+)+)(?:\|\s+)((?:[0-9]+\s+)+)"
 
-
 class Card:
 
     card_nr: int
 
     def __init__(self, data):
         self.raw_data = data
-        self.cardnr, self.winning = self.parse_input(data)
+        self.cardnr, self.winning, self.you_have = self.parse_input(data)
 
     def parse_input(self, input_data: str):
+        def parse_to_list_of_int(list_of_nr_str: list[str]) -> list[int]:
+            return list(map(int, list_of_nr_str))
+
         matches = re.match(PATTERN, input_data)
         nr = int(matches.group(1))
-        return nr, 0
+        winning = parse_to_list_of_int(matches.group(2).split())
+        you_have = parse_to_list_of_int(matches.group(3).split())
+        return nr, winning, you_have
 
 def solve_a(data):
     return None
