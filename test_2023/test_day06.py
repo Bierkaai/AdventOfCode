9ffa8fac9a5469be86decf360aa363f114f1a8f3
Coen Jonker <coen.j.jonker@gmail.com>
2023-12-06 16:06:34 +0100

    Test for number of races. Fails

--- test_2023/test_day06.py
+++ test_2023/test_day06.py
@@ -2,7 +2,7 @@ import unittest
 
 from aocd.models import Puzzle
 
-from y2023.day02 import solve_a, solve_b
+from y2023.day06 import solve_a, solve_b, BoatRaceBuilder
 
 YEAR = 2023
 DAY = 6
@@ -12,6 +12,17 @@ EXAMPLE_RESULT_B = 281
 puzzle = Puzzle(year=YEAR, day=DAY)
 EXAMPLE_DATA = puzzle.example_data
 
+EXAMPLE_RACECOUNT = 3
+
+class TestParsing(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.boatraces = BoatRaceBuilder(EXAMPLE_DATA)
+
+    def testRaceCountCorrect(self):
+        """There should be three races in the parsed data"""
+        self.assertEqual(self.boatraces, EXAMPLE_RACECOUNT)
 
 class TestDay01(unittest.TestCase):
 
