3e8bffe7db3e1b5dca5521fa924c573a5b3b094b
Coen Jonker <coen.j.jonker@gmail.com>
2023-12-06 08:48:38 +0100

    Almanac implementation added, parsing test succeeds.

--- y2023/day05.py
+++ y2023/day05.py
@@ -1,7 +1,22 @@
+import re
+
 from aocd.models import Puzzle
 
 DAY = 5
 YEAR = 2023
+
+
+class Almanac:
+
+    def __init__(self, raw_data):
+        self.raw_data = raw_data
+        self.parse(raw_data)
+
+    def parse(self, raw_data):
+        for line in raw_data.split('\n'):
+            if line.startswith("seeds"):
+                match = re.match(r"(?:seeds\:)((?:\s*[0-9]+)+)", line)
+                self.seed_list = list(map(int, match.group(1).split()))
 
 
 def solve_a(data):
