da4287e9c5185618d556e84cf6f49cc68da8da16
Coen Jonker <coen.j.jonker@gmail.com>
2023-12-06 16:42:30 +0100

    Parser for boatraces, parsing tests pass

--- y2023/day06.py
+++ y2023/day06.py
@@ -1,15 +1,27 @@
+import re
+
 from aocd.models import Puzzle
 
 DAY = 6
 YEAR = 2023
 
 
+def build_boatracelist(raw_data):
+    for line in raw_data.split("\n"):
+        if line.startswith("Time:"):
+            times = re.findall(r"[0-9]+", line)
+        elif line.startswith("Distance:"):
+            dists = re.findall(r"[0-9]+", line)
+    return tuple(zip(map(int, times), map(int, dists)))
+
+
 def solve_a(data):
-    return None
+    races = build_boatracelist(data)
+    return 0
 
 
 def solve_b(data):
-    return None
+    return 0
 
 
 if __name__ == "__main__":
